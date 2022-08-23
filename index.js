5c5c82b63ab54f7862bd63c62a0779d4b3c4d90c
Tonny Blair Okumu <hellotony2034@gmail.com>
2022-08-23 09:09:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (feet >= 2500) {
+    return 'No can do.'
+  }else {
+    return "I will gladly take your thirty bucks."
+  }
+}
+scuberGreetingForFeet(199)
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  if(city === "NYC") {
+    return "Ok, sounds good.";
+  } else   { return "No go."
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === 'generous') {
+    return "Thank you so much.";
+  } else if (tip === 'not as generous') {
+    return 'Thank you.';
+} else if (tip === 'thanks for everything') {
+  return 'Bye.'
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
